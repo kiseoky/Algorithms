8e689dcfd5db9a13811f71529607f0b13d0af6b7
Kiseok <rltjr1092@gmail.com>
2022-10-08 15:37:24 +0900

    updated at 2022. 10. 8. 오후 3:37:23

--- baekjoon/4195/solution.py
+++ baekjoon/4195/solution.py
@@ -1,31 +1,28 @@
 // [문제 링크]: https://www.acmicpc.net/problem/4195
 
-import sys
-input = sys.stdin.readline
+from collections import defaultdict
+​
 t = int(input())
 ​
 for _ in range(t):
     f = int(input())
     users = {}
-    cnt = {}
+    cnt = defaultdict(lambda: 1)
 ​
     def find(a):
+        if a not in users:
+            users[a] = a
+​
         if a == users[a]:
             return a
         users[a] = find(users[a])
         return users[a]
 ​
     for _ in range(f):
-        a, b = map(str, input().split())
-        if a not in users:
-            users[a] = a
-            cnt[a] = 1
-        if b not in users:
-            users[b] = b
-            cnt[b] = 1
-​
+        a, b = input().split()
         x, y = find(a), find(b)
         if x != y:
-            users[y] = x
-            cnt[x] += cnt[y]
-        print(cnt[find(a)])
+            x, y = find(a), find(b)
+            users[x] = y
+            cnt[y] += cnt[x]
+        print(cnt[y])
