32a58daba2f77ef4167645e30571aeea7c614982
Kiseok <rltjr1092@gmail.com>
2022-12-23 19:36:46 +0900

    updated at 2022. 12. 23. 오후 7:36:44

--- programmers/여행경로/solution.py
+++ programmers/여행경로/solution.py
@@ -1,22 +1,25 @@
 // [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/43164
 
+from collections import defaultdict
+
 def solution(tickets):
-    n = len(tickets)
-    answer = ['ZZZ']*(n+1)
-    visited = set()
-    
-    def dfs(curr, route):
-        nonlocal answer
-        if len(route) == n+1:
-            answer = min(answer, route)
-            return
-        
-        for i, (start, depart) in enumerate(tickets):
-            if start != curr or i in visited:
-                continue
-            visited.add(i)
-            dfs(depart, route+[depart])
-            visited.remove(i)
-            
-    dfs("ICN", ["ICN"])
-    return answer
+    path = []
+
+    graph = defaultdict(list)
+    for (start, end) in tickets:
+        graph[start].append(end)
+
+    for airport in graph.keys():
+        graph[airport].sort(reverse=True)
+
+    stack = ["ICN"]
+    while stack:
+        top = stack.pop()
+
+        if top not in graph or not graph[top]:
+            path.append(top)
+        else:
+            stack.append(top)
+            stack.append(graph[top].pop())
+
+    return path[::-1]
